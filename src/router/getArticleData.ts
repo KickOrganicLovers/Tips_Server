fea815b08b9d7539637bf6eb1ed83eb9e4dc4bf1
Shaltear-Lychee <49912764+Shaltear-Lychee@users.noreply.github.com>
2024-04-08 06:06:41 +0900

    Added my app a function to send data to client

--- src/router/getArticleData.ts
+++ src/router/getArticleData.ts
@@ -5,8 +5,9 @@ import {ArticleScheme} from "../types";
 const getArticleData = e.Router()
 
 getArticleData.get('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000')
     read<ArticleScheme>().then((val) => {
-        res.send(val[1].title)
+        res.json(val)
     })
 })
 
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,6 @@ server.use('/insert', insertArticleData)
 server.use('/get', getArticleData)
 
 server.get('/', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000')
     res.send('Hello World!')
 })
 
